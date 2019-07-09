1416bf511b9069543ac0418cd00d37dbf8af1a40
mk4j <nick.latyshev@gmail.com>
2019-07-09 06:11:40 +0100

    to test

--- src/main/java/com/mk/pp/FlatWatcher.java
+++ src/main/java/com/mk/pp/FlatWatcher.java
@@ -12,7 +12,7 @@ public class FlatWatcher
     static String kom = "https://www.avito.ru/sankt-peterburg/kvartiry/sdam/na_dlitelnyy_srok?user=1&s_trg=4&metro=168";
 
     public static void main(String[] args) throws IOException, InterruptedException{
-        Watcher komWatcher = new Watcher(kom);
+        //Watcher komWatcher = new Watcher(kom);
 
 
         ApiContextInitializer.init();
@@ -23,7 +23,7 @@ public class FlatWatcher
             e.printStackTrace();
         }
 
-        komWatcher.monitor();
+        //komWatcher.monitor();
 
 
 
